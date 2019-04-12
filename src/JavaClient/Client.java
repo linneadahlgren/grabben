b706c1834ea9e29d03c16cbfb5e6b6eb313541f2
Tove Rumar <toverumar@gorans-air.clients.internal.mah.se>
2019-04-12 09:41:55 +0200

    skapat klientens skelett

---
+++ src/JavaClient/Client.java
@@ -0,0 +1,30 @@
+package JavaClient;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+	private Socket socket;
+	private DataOutputStream output;
+	
+	
+	public Client(Socket socket) {
+		this.socket=socket;
+	}
+	
+	public DataOutputStream getOutputStream() {
+		if(output==null) {
+			try {
+				output=new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			}catch(IOException e) {
+				e.printStackTrace();
+				System.out.print("Cannot set up outputstream");
+			}
+		}return output;
+	}
+	
+
+}
