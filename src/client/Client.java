4b4318d101ce7b4ee9cb0ff93e1a0e4245f34cf8
Tove Rumar <toverumar@gorans-air.clients.internal.mah.se>
2019-04-12 13:25:02 +0200

    Update Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,8 @@
 package client;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,6 +11,7 @@ import java.net.Socket;
 public class Client {
 	private Socket socket;
 	private DataOutputStream output;
+	private DataInputStream input;
 	
 	
 	public Client(Socket socket) {
@@ -23,10 +26,21 @@ public class Client {
 			}catch(IOException e) {
 				e.printStackTrace();	
 			}
-			
 		}
 		
 		return output;
+	}
+	public DataInputStream getInputStream() {
+		
+		if(input==null) {
+			try {
+				input=new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			}catch(IOException e) {
+				e.printStackTrace();
+				
+			}
+		}
+		
 	}
 	
 
