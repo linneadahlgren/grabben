54d6817b3be8e1c55f7c5d8840c987472a0f0642
Tove Rumar <toverumar@gorans-air-2.clients.internal.mah.se>
2019-04-30 09:36:28 +0200

    Update KloProgram1.01.ino

--- Styrenhet/KloProgram1.01/KloProgram1.01.ino
+++ Styrenhet/KloProgram1.01/KloProgram1.01.ino
@@ -6,6 +6,9 @@ int enableMotors=A0;
 int xMotor1=A1;
 int xMotor2=A2;
 
+const int halt=0;
+const int fast=200;
+const int intermediate=150;
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 byte serverIp[] = {192,168,20,195};
@@ -53,40 +56,58 @@ void sendMsg(String msg) {
      client.flush();
     }
    }
- void loop() {
+
+ void halt(){
+  
+  
+ }
+ void up(){
+  analogWrite(pwmXY, fast);
+  digitalWrite(xMotor1,HIGH);
+  digitalWrite(xMotor,LOW);
+  
+ }
+
+ void forward(){
+  
+ }
+ void right(){
+  
+}
+void left (){
+  
+}
+void loop() {
  if (client.connected() == true) {
     String command = client.readString();
     if (command == "UP") {
     Serial.print(command);
-    
-      digitalWrite(LED1, HIGH);
-      //delay (1000);
-     // digitalWrite(LED1, LOW);
-     // delay(1000);
+      
+     
     }
     if (command == "DOWN") {
-      digitalWrite(LED2, HIGH);
-      delay (1000);
-      digitalWrite(LED2, LOW);
-      delay(1000);
+     // digitalWrite(LED2, HIGH);
+     // delay (1000);
+     // digitalWrite(LED2, LOW);
+     // delay(1000);
     }
       if (command == "LEFT") {
-      digitalWrite(LED3, HIGH);
-      delay (1000);
-      digitalWrite(LED3, LOW);
+      //digitalWrite(LED3, HIGH);
+      //delay (1000);
+      //digitalWrite(LED3, LOW);
   //    delay(100);
   //    digitalWrite(LED1, HIGH);
   //    delay (200);
   //    digitalWrite(LED1, LOW);
-      delay(1000);
+     // delay(1000);
     }
       if (command == "RIGHT") {
-      digitalWrite(LED4, HIGH);
+      //digitalWrite(LED4, HIGH);
   //    digitalWrite(LED1, HIGH);
-      delay (1000);
-      digitalWrite(LED4, LOW);
+     // delay (1000);
+     // digitalWrite(LED4, LOW);
   //    digitalWrite(LED1, LOW);
-      delay(1000);
+     // delay(1000);
     }
         Serial.println(command);
  }
