030ff7c46d0a67334eeded0a7370df46268740d0
linnea <dahlgren99@gmail.com>
2019-05-29 09:17:28 +0200

    restructured the main folder
    put all the files for the eclipse project in the folder "server"

--- src/client/ClientViewer.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-public interface ClientViewer {
-	public void up();
-	public void down();
-	public void left();
-	public void right();
-	public void grab();
-}
--- src/grabben/Test.java
+++
@@ -1,8 +0,0 @@
-package grabben;
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("Välkommen till grabben.");
-	}
-}
